3e48096346ee7a98276735408525b25001112a1b
mertflixs <mertflix@outlook.com>
2023-02-24 00:24:44 +0300

    rum

---
+++ cpp_module01/ASpell.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+# include <iostream>
+# include "ATarget.hpp"
+
+class ATarget;
+
+class ASpell{
+	private:
+		std::string name;
+		std::string effects;
+	public:
+		ASpell();
+		ASpell(std::string const &name, std::string const &effects);
+		ASpell(ASpell const &assign);
+		ASpell &operator=(ASpell const &assign);
+		virtual ~ASpell();
+		std::string const &getName() const;
+		std::string const &getEffects() const;
+		void launch(ATarget const &atarget_ref) const;
+		virtual ASpell *clone() const = 0;
+
+};
---
+++ cpp_module02/Warlock.cpp
@@ -0,0 +1,37 @@
+#include "Warlock.hpp"
+
+Warlock::Warlock(std::string const &name, std::string const &title)
+{
+    this->name = name;
+    this->title = title;
+    std::cout << this->name << ": This looks like another boring day.\n";
+}
+
+Warlock::~Warlock()
+{
+    std::cout << this->name << ": My job here is done!\n";
+}
+
+std::string const &Warlock::getName() const { return (this->name);}
+std::string const &Warlock::getTitle() const { return (this->title);}
+
+void Warlock::setTitle(std::string const &title) { this->title = title;}
+
+void Warlock::introduce() const { std::cout << this->name << ": I am " << this->name << ", " << this->title << "!\n";}
+
+void Warlock::learnSpell(ASpell* spell_ptr)
+{
+	book.learnSpell(spell_ptr);
+}
+
+void Warlock::forgetSpell(std::string spell_name)
+{
+	book.forgetSpell(spell_name);
+}
+
+void Warlock::launchSpell(std::string spell_name, ATarget const &target_ref)
+{
+	ASpell *temp = book.createSpell(spell_name);
+	if (temp)
+		temp->launch(target_ref);
+}
